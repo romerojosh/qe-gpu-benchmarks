8a2706de1576837e1c146babda1402de51bd30e1
Josh Romero <romero.josh@gmail.com>
2018-01-23 00:01:02 +0100

    Adding sample PizDaint AUSURF112 result and PizDaint make.inc

---
+++ AUSURF112/PizDaint/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=gpu
+#SBATCH --contiguous
+#SBATCH --time=00:10:00
+
+# Load required modules
+module load PrgEnv-pgi
+module load intel  
+module load cudatoolkit 
+
+if [ -z "${QE_EXE}" ]; then echo "ERROR: Please export QE_EXE before running script." ; exit; fi
+
+# QE run parameters
+NGPU=8
+NPOOL=2
+
+# Node-specific parameters
+GPU_PER_SOCKET=1
+CORES_PER_SOCKET=12
+
+NCORE_PER_RANK=$((${CORES_PER_SOCKET}/${GPU_PER_SOCKET}))
+
+export OMP_NUM_THREADS=${NCORE_PER_RANK}
+export MKL_NUM_THREADS=${NCORE_PER_RANK}
+
+srun -n ${NGPU} -c ${CORES_PER_SOCKET} --cpu_bind=none ${QE_EXE} -input ../pw.in -npool ${NPOOL} | tee output_${NGPU}GPU_${NPOOL}POOL.txt
